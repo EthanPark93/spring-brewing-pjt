e1252c193454d72df05021a3642c3b56912c5d2b
EthanPark93 <katsube01@gmail.com>
2023-11-06 01:50:43 +0900

    daily_commit

---
+++ homebodybrewery/src/main/java/com/ethan/homebodybrewery/HomebodybreweryApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.homebodybrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebodybreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebodybreweryApplication.class, args);
+	}
+
+}
